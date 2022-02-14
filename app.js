2f0d580f0fbc779ea8686bf2be00d0410e5d63b4
Lamer217 <males.public@gmail.com>
2022-02-14 15:33:11 +0100

    Resolve double folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require('dotenv/config');
+require('dotenv').config();
 
 // ℹ️ Connects to the database
 require('./db');
@@ -20,26 +20,25 @@ require('./config')(app);
 
 // default value for title local
 const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
-
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        cookie: {maxAge: 1000 * 60 * 60 * 24},
-        resave: true,
-        saveUninitialized: false,
-        store: MongoStore.create({
-            mongoUrl: process.env.MONGODB_URI
-        })
-    })
-)
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
@@ -47,7 +46,6 @@ app.use('/', index);
 
 const auth = require('./routes/auth');
 app.use('/', auth);
-
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
---
+++ todo.md
@@ -0,0 +1,5 @@
+#Todo
+Profile page
+Ability to write tweets and to show them on your profile page
+Create a model for twwets or edit the user model to store tweets
+Add timestamp to tweets (so that you can see when those were written)
