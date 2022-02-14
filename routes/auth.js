d277eb41651517e79a0f09c33b8d072c6091f894
Lamer217 <males.public@gmail.com>
2022-02-14 20:33:32 +0100

    Add email input for signup fform

--- routes/auth.js
+++ routes/auth.js
@@ -1,72 +1,57 @@
-const { post } = require(".");
-const bcrypt = require('bcryptjs')
-const User = require('../models/User.js')
+const { post } = require('.');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User.js');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
 router.get('/signup', (req, res, next) => {
   res.render('signup', {
-      title: 'Sign Up'
+    title: 'Twitter | Sign Up',
   });
 });
 
 router.get('/login', (req, res, next) => {
-    res.render('login', {
-        title: 'Log In'
-    });
+  res.render('login', {
+    title: 'Twitter | Log In',
+  });
 });
-
-
 
 router.post('/signup', (req, res, next) => {
-    const { username, password } = req.body
-    if(password.length < 6){
-        res.render('signup', {message: 'Your password should be min 6 character long'})
-        return
+  const { username, password, email } = req.body;
+  User.findOne({ username: username }).then(userFromDB => {
+    if (userFromDB) {
+      res.render('signup', { message: 'Username is already taken' });
+    } else {
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+      User.create({ username, password: hash, email })
+        .then(createdUser => {
+          console.log(createdUser);
+          res.redirect('/login');
+        })
+        .catch(err => next(err));
     }
-    if (username.length === 0){
-        res.render('signup', {message: 'Your username is not valid'})
-        return
-    }
-    User.findOne({username: username})
-    .then(userFromDB => {
-        if (userFromDB !== null) {
-            res.render('signup', {message: 'Username is already taken'})
-        } 
-        else {
-            const salt = bcrypt.genSaltSync()
-            const hash = bcrypt.hashSync(password, salt)
-            User.create({ username, password: hash })
-            .then(createdUser => {
-                console.log(createdUser)
-                res.redirect('/login')
-            })
-            .catch(err => next(err))
-        }
-    })
-
+  });
 });
 
-router.post('/login', (req,res,next) => {
-    console.log('this body', req.body)
-    const {username, password} = req.body
-    User.findOne({username: username})
-    .then(userFromDB => {
-        if (userFromDB === null) {
-            res.render('login', {message: 'Invalid'})
-            return 
-        }
-        if(bcrypt.compareSync(password, userFromDB.password)){
-            req.session.user = userFromDB
-            res.redirect('/profile')
-        }
-    })
-})
+router.post('/login', (req, res, next) => {
+  console.log('this body', req.body);
+  const { username, password } = req.body;
+  User.findOne({ username: username }).then(userFromDB => {
+    if (userFromDB === null) {
+      res.render('login', { message: 'Invalid credentials' });
+      return;
+    }
+    if (bcrypt.compareSync(password, userFromDB.password)) {
+      req.session.user = userFromDB;
+      res.redirect('/profile');
+    }
+  });
+});
 
 router.get('/logout', (req, res, next) => {
-    req.session.destroy()
-    res.render('index')
-})
-
+  req.session.destroy();
+  res.render('index');
+});
 
 module.exports = router;
