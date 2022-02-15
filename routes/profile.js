f2618fa5602a33ee4e8410b892752a93b2bfe45d
Lamer217 <males.public@gmail.com>
2022-02-15 15:03:56 +0100

    Add ability to write personal tweets

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const Tweet = require('../models/Tweet');
+const User = require('../models/User');
 
 //middleware
 function loginCheck() {
@@ -11,9 +13,24 @@ function loginCheck() {
   };
 }
 
-router.get('/profile', loginCheck(), (req, res, next) => {
+router.get('/', loginCheck(), (req, res, next) => {
   const user = req.session.user;
-  res.render('profile', { user: user });
+  res.render('profile', { user: user, title: `${user.username}'s profile` });
+});
+
+router.post('/new-tweet', (req, res, next) => {
+  const { tweet } = req.body;
+  Tweet.create({ content: tweet })
+    .then(newTweet => {
+      const tweetId = newTweet._id;
+      const userId = req.session.user._id;
+      User.findByIdAndUpdate(userId, { $push: { tweets: tweetId } }).then(
+        editedUser => console.log(editedUser)
+      );
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
